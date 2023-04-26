390f197e523f14a539013ff8cfd8abe8b725c735
TesliukK <teslukkirill@gmail.com>
2023-04-26 18:09:46 +0300

    hw3

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+/* eslint-env node */
+module.exports = {
+  extends: [
+    "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  root: true,
+  rules: {
+    "no-unused-vars": ["error", { argsIgnorePattern: "req|res|next" }],
+  },
+};
