a9048d7bedbc5ed66f6fb6b88a98e52390eaa047
TesliukK <teslukkirill@gmail.com>
2023-05-06 20:01:36 +0300

    hw4

---
+++ dist/types/users.types.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.EGenders = void 0;
+var EGenders;
+(function (EGenders) {
+    EGenders["female"] = "female";
+    EGenders["male"] = "male";
+    EGenders["mixed"] = "mixed";
+})(EGenders = exports.EGenders || (exports.EGenders = {}));
