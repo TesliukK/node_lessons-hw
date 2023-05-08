dafc7d616acc5578cd33dbed04294a849337d25d
TesliukK <115839561+TesliukK@users.noreply.github.com>
2023-05-08 11:41:03 +0300

    Delete dist directory

--- dist/types/users.types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EGenders = void 0;
-var EGenders;
-(function (EGenders) {
-  EGenders["female"] = "female";
-  EGenders["male"] = "male";
-  EGenders["mixed"] = "mixed";
-})((EGenders = exports.EGenders || (exports.EGenders = {})));
