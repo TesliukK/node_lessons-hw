d7bde65d6160f8d9ea4c52f46d2f58c5f63f93e9
kirill <teslukkirill@gmail.com>
2023-05-24 19:29:06 +0300

    9hw

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -3,14 +3,6 @@ import { User } from "../models";
 import { IPaginationResponse, IQuery, IUser } from "../types";
 
 class UserService {
-  public async getAll(): Promise<IUser[]> {
-    try {
-      return User.find();
-    } catch (e) {
-      throw new ApiError(e.message, e.status);
-    }
-  }
-
   public async getWithPagination(
     query: IQuery
   ): Promise<IPaginationResponse<IUser>> {
@@ -50,7 +42,23 @@ class UserService {
 
   public async getById(id: string): Promise<IUser> {
     try {
-      return User.findById(id);
+      return User.findById(id).lean();
+    } catch (e) {
+      throw new ApiError(e.message, e.status);
+    }
+  }
+
+  public async update(userId: string, data: Partial<IUser>): Promise<void> {
+    try {
+      return await User.findByIdAndUpdate(userId, data, { new: true });
+    } catch (e) {
+      throw new ApiError(e.message, e.status);
+    }
+  }
+
+  public async delete(userId: string): Promise<void> {
+    try {
+      await User.deleteOne({ _id: userId });
     } catch (e) {
       throw new ApiError(e.message, e.status);
     }
